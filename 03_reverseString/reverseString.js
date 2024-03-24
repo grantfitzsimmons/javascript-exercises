fce1e2292ebd526a3f3f9d8d64e3e9e057d4465f
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2024-03-23 21:52:14 -0500

    Add function to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(userString) {
+    let userArray = userString.split("");
+    userArray = userArray.reverse();
+    return userArray.join("");
 };
 
 // Do not edit below this line
