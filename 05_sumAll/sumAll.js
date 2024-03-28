97ac94609342caae8edce4454dec3f2156dcec4b
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2024-03-27 20:40:09 -0500

    Add checks for negative and non-integer numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
 const sumAll = function(min,max) {
     let sum = 0;
+    if (!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0) {
+        return "ERROR"
+    }
     if (min < max) {
         for (min; min <= max; min++) {
             sum += min; 
